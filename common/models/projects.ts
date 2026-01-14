ef6e0926f94621c13592dba423fe052feb58d510
Stephen Patane <patane97@gmail.com>
2026-01-14 18:03:40 +1100

    Fixing projects file name (case sensitive)

---
+++ common/models/projects.ts
@@ -0,0 +1,20 @@
+export const types = ["cycle", "expansion"] as const;
+export type Type = typeof types[number];
+export type Code = `${number}`;
+
+export interface IProject {
+    number: number,
+    name: string,
+    code: string,
+    active: boolean,
+    type: Type,
+    script: string,
+    perFaction: number,
+    neutral: number,
+    version: number,
+    milestone: number,
+    formUrl: string,
+    emoji?: string,
+    created: Date,
+    updated: Date
+}
